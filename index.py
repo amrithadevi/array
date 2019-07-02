413a7597b6ac5eb5832b8b5d640af9de902b8439
Sangeetha.M <sangeethameganathan98@gmail.com>
2019-07-02 16:21:11 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,11 @@
+  um=int(input())
+  l=[int(x) for x in input().split()[:num]]
+  nlj=[]
+  for i in range(0,int(len(l))):
+     if(i==l[i]):
+         nlj.append(i)
+  if(int(len(nlj)))!=0:
+        for u in nlj:
+            print(u,end=" ")
+  else:
+      print(-1)
