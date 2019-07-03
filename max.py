59355776098e75eb7d861d029e145a1d69a11ac5
Sangeetha.M <sangeethameganathan98@gmail.com>
2019-07-03 15:45:49 +0530

    Update max.py

--- max.py
+++ max.py
@@ -1,10 +1,10 @@
- n = input()
- array = map(int,raw_input().split())
- arr = []
- for i in range(0,2):
-     ans = 0
-     for i in range(0,n,2):
-         ans = ans + array[i]
-     arr.append(ans)
- arr.sort()
- print arr[-1]
+  n = input()
+  array = map(int,raw_input().split())
+  arr = []
+  for i in range(0,2):
+      ans = 0
+      for i in range(0,n,2):
+          ans = ans + array[i]
+      arr.append(ans)
+  arr.sort()
+  print arr[-1]
