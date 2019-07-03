808bac25f35d37270d84478750bfe6d10a3787ed
Sangeetha.M <sangeethameganathan98@gmail.com>
2019-07-03 15:43:22 +0530

    Create max.py

---
+++ max.py
@@ -0,0 +1,10 @@
+ n = input()
+ array = map(int,raw_input().split())
+ arr = []
+ for i in range(0,2):
+     ans = 0
+     for i in range(0,n,2):
+         ans = ans + array[i]
+     arr.append(ans)
+ arr.sort()
+ print arr[-1]
